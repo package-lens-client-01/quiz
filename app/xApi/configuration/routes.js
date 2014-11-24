1d7f977d7e9b6081e0b32243957f5e2a24e10209
Romsters <petriv.roma@gmail.com>
2014-11-24 14:37:55 +0200

    remove ie9 spec

--- app/xApi/configuration/routes.js
+++ app/xApi/configuration/routes.js
@@ -8,12 +8,6 @@
             hideNav: true
         },
         {
-            route: 'xapinotsupported',
-            moduleId: 'xApi/viewmodels/xAPINotSupported',
-            title: 'Progress tracking cannot be established',
-            hideNav: true
-        },
-        {
             route: 'xapierror(/:backUrl)',
             moduleId: 'xApi/viewmodels/xAPIError',
             title: 'xAPI Error',
