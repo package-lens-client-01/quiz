50eed597aadf89f7653ced783b0207dea3ccf68b
NadiaGamula <nadia.zakharova@gmail.com>
2014-11-28 19:28:36 +0200

    Fix bug with settings

--- settings/js/settings.js
+++ settings/js/settings.js
@@ -487,11 +487,12 @@
         url: settingsURL,
         dataType: "json",
         success: function(json) {
+            var defaultSettings = { logo: {}, xApi: { lrs: { credentials: {} } }, masteryScore: {} };
             var settings;
             try {
-                settings = JSON.parse(json);
+                settings = JSON.parse(json.settings) || defaultSettings;
             } catch (e) {
-                settings = { logo: {}, xApi: { lrs: { credentials: {} } }, masteryScore: {} };
+                settings = defaultSettings;
             }
             viewModel.trackingData.enableXAPI(settings.xApi.enabled || false);
             viewModel.trackingData.selectedLrs(settings.xApi.selectedLrs || 'default');
