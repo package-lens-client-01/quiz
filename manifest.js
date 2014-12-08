3aa968aa92fc0dc05bc42cf0a90ebef485df74c9
MonteRio <plo.91ua@gmail.com>
2014-12-08 17:59:07 +0200

    Redesign templates settings(Quiz)

---
+++ manifest.js
@@ -0,0 +1,9 @@
+﻿{
+    "name": "Quiz",
+    "shortDescription": "Use this when you want to create an assessment",
+    "image": "img/main.png",
+    "previewImages": [
+        "img/previewImages/previewQuestionView.png"
+    ],
+    "settingsUrl": "settings/settings.html"
+}
